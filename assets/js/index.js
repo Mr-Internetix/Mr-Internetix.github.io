c64396ff6d96d32c829ac857b4e9722c5e8c4cae
Ajit Yadav <69528685+Mr-Internetix@users.noreply.github.com>
2021-04-13 09:49:37 +0530

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,11 +21,11 @@ function getData() {
   img.innerHTML = "<img id='logo'src ='./assets/images/logo.png'/>";
   img.classList.remove("loading");
   img.style.background = "none";
-  img.style.overflow ="visible";
+  img.style.overflow = "visible";
 
   let logo = document.getElementById("logo");
   // console.log(logo);
-  logo.style.borderRadius="50%";
+  logo.style.borderRadius = "50%";
 
   let name = document.querySelector(".name");
   name.innerHTML = "  Ajit Yadav";
@@ -46,18 +46,21 @@ function getData() {
   line2.innerHTML = " looking forward to work with innovative";
   line2.classList.remove("loading");
   line2.style.background = "none";
-  
 
   let line3 = document.querySelector(".line-3");
-  line3.innerHTML =" and hardworking people.";
+  line3.innerHTML = " and hardworking people.";
   line3.classList.remove("loading");
   line3.style.background = "none";
 
   let btn1 = document.querySelector(".btn-1");
-  btn1.innerHTML = "<a class='card-button' href='https://github.com/Mr-Internetix'>checkout github </a>";
-  // btn1.classList.remove("loading");
-  // btn1.style.background = "none";
-  
+  btn1.innerHTML =
+    "<a class='card-button' href='https://github.com/Mr-Internetix'>Checkout Projects</a>";
+  btn1.classList.remove("loading");
+  btn1.style.background = "none";
+
+
+  let card = document.querySelector(".card");
+  card.style.border="none";
   // btn1.classList.add("card-button");
 
   // let btn2 = document.querySelector(".btn-2");
@@ -65,6 +68,9 @@ function getData() {
   // btn2.classList.remove("loading");
   // btn2.style.background = "none";
 
+  // let projects = document.querySelector(".card-button");
+  // projects.addEventListener("mouseover",function(event){
+  //   // event.target.classList.add("loading")
+  // });
+
 }
-
-
